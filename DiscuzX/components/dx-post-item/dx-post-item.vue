d672aa8270b88aa4a8e20335f1ca6f1b6aa86d9e
姜彦汐 <jiangyanxi@live.com>
2021-06-07 16:21:51 +0800

    up

---
+++ DiscuzX/components/dx-post-item/dx-post-item.vue
@@ -0,0 +1,20 @@
+<template>
+	<view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"dx-post-item",
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style>
+
+</style>
--- DiscuzX/pages/user/user.vue
+++ DiscuzX/pages/user/user.vue
@@ -1,5 +1,43 @@
 <template>
 	<view>
+		<scroll-view scroll-y="true" class="has-tabbar">
+			<view class="u-flex user-box u-p-l-30 u-p-r-20 u-p-b-30 margin-top">
+				<view class="u-m-r-10">
+					<u-avatar :src="pic" size="140"></u-avatar>
+				</view>
+				<view class="u-flex-1">
+					<view class="u-font-18 u-p-b-20">uView ui</view>
+					<view class="u-font-14 u-tips-color">微信号:helang_uView</view>
+				</view>
+				<view class="u-m-l-10 u-p-10">
+					<u-icon name="scan" color="#969799" size="28"></u-icon>
+				</view>
+				<view class="u-m-l-10 u-p-10">
+					<u-icon name="arrow-right" color="#969799" size="28"></u-icon>
+				</view>
+			</view>
+
+			<view class="u-m-t-20">
+				<u-cell-group>
+					<u-cell-item icon="rmb-circle" title="支付"></u-cell-item>
+				</u-cell-group>
+			</view>
+
+			<view class="u-m-t-20">
+				<u-cell-group>
+					<u-cell-item icon="star" title="收藏"></u-cell-item>
+					<u-cell-item icon="photo" title="相册"></u-cell-item>
+					<u-cell-item icon="coupon" title="卡券"></u-cell-item>
+					<u-cell-item icon="heart" title="关注"></u-cell-item>
+				</u-cell-group>
+			</view>
+
+			<view class="u-m-t-20">
+				<u-cell-group>
+					<u-cell-item @click="goto('../setting/setting')" icon="setting" title="设置"></u-cell-item>
+				</u-cell-group>
+			</view>
+		</scroll-view>
 		<dx-tabbar :currentTab="4"></dx-tabbar>
 	</view>
 </template>
@@ -8,15 +46,21 @@
 	export default {
 		data() {
 			return {
-				
+
 			}
 		},
 		methods: {
-			
+			goto(url) {
+				uni.navigateTo({
+					url
+				});
+			}
 		}
 	}
 </script>
 
 <style>
-
+	.has-tabbar {
+		height: calc(100vh - 94px);
+	}
 </style>
