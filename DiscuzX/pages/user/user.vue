57e671b7b5d72948ecc214b8c81a5665c55c7c4e
姜彦汐 <jiangyanxi@live.com>
2021-06-07 16:52:56 +0800

    up

--- DiscuzX/pages/user/user.vue
+++ DiscuzX/pages/user/user.vue
@@ -9,26 +9,21 @@
 					<view class="u-font-18 u-p-b-20">uView ui</view>
 					<view class="u-font-14 u-tips-color">微信号:helang_uView</view>
 				</view>
-				<view class="u-m-l-10 u-p-10">
-					<u-icon name="scan" color="#969799" size="28"></u-icon>
-				</view>
-				<view class="u-m-l-10 u-p-10">
-					<u-icon name="arrow-right" color="#969799" size="28"></u-icon>
-				</view>
 			</view>
 
 			<view class="u-m-t-20">
 				<u-cell-group>
-					<u-cell-item icon="rmb-circle" title="支付"></u-cell-item>
+					<u-cell-item @click="goto('./friend/friend')" icon="man-add" title="我的好友"></u-cell-item>
+					<u-cell-item @click="goto('./following/following')" icon="bookmark" title="我的关注"></u-cell-item>
+					<u-cell-item @click="goto('./followers/followers')" icon="heart" title="我的粉丝"></u-cell-item>
 				</u-cell-group>
 			</view>
 
 			<view class="u-m-t-20">
 				<u-cell-group>
-					<u-cell-item icon="star" title="收藏"></u-cell-item>
-					<u-cell-item icon="photo" title="相册"></u-cell-item>
-					<u-cell-item icon="coupon" title="卡券"></u-cell-item>
-					<u-cell-item icon="heart" title="关注"></u-cell-item>
+					<u-cell-item @click="goto('./post/post')" icon="file-text" title="我的发表"></u-cell-item>
+					<u-cell-item @click="goto('./reply/reply')" icon="edit-pen" title="我的回复"></u-cell-item>
+					<u-cell-item @click="goto('./star/star')" icon="heart" title="我的收藏"></u-cell-item>
 				</u-cell-group>
 			</view>
 
