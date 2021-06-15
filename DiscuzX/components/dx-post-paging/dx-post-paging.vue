7fc8bcfdcccc5a40c3c9c1e1e94e8006a60c9101
姜彦汐 <jiangyanxi@live.com>
2021-06-15 16:07:53 +0800

    up

---
+++ DiscuzX/components/dx-post-paging/dx-post-paging.vue
@@ -0,0 +1,101 @@
+<template>
+	<view class="content">
+		<z-paging ref="paging" autowire-list-name="zList" autowire-query-name="zQuery">
+			<view class="cu-list menu-avatar">
+				<view v-for="(item,index) in zList" :key="item.topic_id" class="cu-item cur margin-tb-sm"
+					@click="$util.helper.goto('/pages/detail/detail?id='+item.topic_id)">
+					<view class="cu-avatar round lg" :style="'background-image:url('+item.userAvatar+');'">
+					</view>
+					<view class="content flex-sub">
+						<view class="text-cut">{{item.title}}</view>
+						<view class="text-cut text-grey text-sm">{{item.subject}}</view>
+						<view class="text-xs flex justify-between">
+							<view class="flex">
+								<view class="text-cut text-green">{{item.user_nick_name}}</view>
+								<view class="text-gray margin-left-xs">{{$u.timeFrom(item.last_reply_date)}}</view>
+							</view>
+							<view class="text-gray">
+								<text class="cuIcon-attentionfill margin-lr-xs"></text> {{item.hits}}
+								<text class="cuIcon-messagefill margin-lr-xs"></text> {{item.replies}}
+							</view>
+						</view>
+					</view>
+				</view>
+			</view>
+		</z-paging>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "dx-post-paging",
+		props: {
+			params: Object,
+			auth: {
+				type: Boolean,
+				default: false
+			},
+			method: {
+				type: String,
+				default: 'get'
+			},
+			tabs: Array,
+			tabKey: String,
+			tabIndex: {
+				type: Number,
+				default: 0
+			},
+			currentIndex: {
+				type: Number,
+				default: 0
+			}
+		},
+		data() {
+			return {
+				zList: [],
+				firstLoaded: false
+			};
+		},
+		watch: {
+			currentIndex: {
+				handler(newVal) {
+					if (newVal === this.tabIndex) {
+						if (!this.firstLoaded) {
+							this.$nextTick(() => {
+								this.$refs.paging.reload();
+							})
+						}
+					}
+				},
+				immediate: true
+			},
+		},
+		methods: {
+			zQuery: async function(page, pageSize) {
+				let _params = {
+					page,
+					pageSize
+				}
+				if (this.tabs && this.tabKey) {
+					_params[this.tabKey] = this.tabs[this.tabIndex][this.tabKey]
+				}
+				const res = await this.$http[this.method]({
+					custom: {
+						auth: this.auth
+					},
+					params: Object.assign(_params, this.params)
+				});
+				this.$refs.paging.complete(res.list);
+				this.firstLoaded = true;
+			},
+		}
+	}
+</script>
+
+<style scoped>
+	.content {
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+	}
+</style>
--- DiscuzX/pages/index/index.vue
+++ DiscuzX/pages/index/index.vue
@@ -2,28 +2,7 @@
 	<view>
 		<dx-navbar title="首页"></dx-navbar>
 		<view class="wrapper">
-			<z-paging ref="paging" autowire-list-name="zList" autowire-query-name="zQuery">
-				<view class="cu-list menu-avatar">
-					<view v-for="(item,index) in zList" :key="item.topic_id" class="cu-item cur">
-						<view class="cu-avatar round lg" :style="'background-image:url('+item.userAvatar+');'">
-						</view>
-						<view class="content flex-sub">
-							<view class="text-cut">{{item.title}}</view>
-							<view class="text-cut text-grey text-sm">{{item.subject}}</view>
-							<view class="text-xs flex justify-between">
-								<view class="flex">
-									<view class="text-cut text-green">{{item.user_nick_name}}</view>
-									<view class="text-gray margin-left-xs">{{$u.timeFrom(item.last_reply_date)}}</view>
-								</view>
-								<view class="text-gray">
-									<text class="cuIcon-attentionfill margin-lr-xs"></text> {{item.hits}}
-									<text class="cuIcon-messagefill margin-lr-xs"></text> {{item.replies}}
-								</view>
-							</view>
-						</view>
-					</view>
-				</view>
-			</z-paging>
+			<dx-post-paging :params="params"></dx-post-paging>
 		</view>
 		<dx-tabbar :currentTab="0"></dx-tabbar>
 	</view>
@@ -33,27 +12,14 @@
 	export default {
 		data() {
 			return {
-				zList: []
+				params: {
+					r: 'forum/topiclist',
+					sortby: 'new',
+					boardId: 0,
+					filterType: 'typeid',
+					filterId: 0
+				}
 			};
-		},
-		methods: {
-			zQuery: async function(page, pageSize) {
-				const res = await this.$http.get({
-					custom: {
-						auth: false
-					},
-					params: {
-						r: 'forum/topiclist',
-						sortby: 'new',
-						boardId: 0,
-						filterType: 'typeid',
-						filterId: 0,
-						page,
-						pageSize
-					}
-				});
-				this.$refs.paging.complete(res.list);
-			},
 		}
 	};
 </script>
