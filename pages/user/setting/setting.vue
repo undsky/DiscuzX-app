6e3a50a742cbd014570d1899994a0ed9f8e5a554
姜彦汐 <jiangyanxi@live.com>
2022-01-14 22:49:01 +0800

    up

--- pages/user/setting/setting.vue
+++ pages/user/setting/setting.vue
@@ -1,22 +1,30 @@
 <template>
 	<view>
-		
+		<u-cell-group>
+			<u-cell-item hover-class="none" title="允许访问位置" icon="map" :arrow="false"><u-switch slot="right-icon" v-model="locationChecked"></u-switch></u-cell-item>
+			<u-cell-item hover-class="none" title="消息通知" icon="volume-up" :arrow="false"><u-switch slot="right-icon" v-model="locationChecked"></u-switch></u-cell-item>
+			<u-cell-item icon="share" title="分享"></u-cell-item>
+			<u-cell-item icon="question-circle" title="意见反馈"></u-cell-item>
+		</u-cell-group>
+		<u-gap :bg-color="$u.color['infoLight']"></u-gap>
+		<u-cell-group>
+			<u-cell-item icon="info-circle" title="用户协议"></u-cell-item>
+			<u-cell-item icon="info-circle" title="版权声明"></u-cell-item>
+			<u-cell-item icon="info-circle" title="隐私政策"></u-cell-item>
+		</u-cell-group>
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
-			}
-		},
-		methods: {
-			
-		}
-	}
+export default {
+	data() {
+		return {
+			locationChecked: true,
+			noticeChecked: true
+		};
+	},
+	methods: {}
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
