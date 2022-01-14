281b8b86215a84fc29698f58b830132f9ad81151
姜彦汐 <jiangyanxi@live.com>
2022-01-14 13:40:56 +0800

    up

--- pages/user/album/show/show.vue
+++ pages/user/album/show/show.vue
@@ -1,22 +1,52 @@
 <template>
-	<view>
-		
-	</view>
+	<z-paging ref="paging" :use-page-scroll="usePageScroll" v-model="photos" autowire-query-name="zQuery">
+		<view class="grid col-2 padding-sm">
+			<view v-for="item in photos" :key="item.pic_id" class="cu-card case">
+				<view class="cu-item shadow">
+					<view
+						@click="preview"
+						:data-pic="item.origin_pic"
+						class="bg-img flex align-end"
+						style="height: 414upx;"
+						:style="{ backgroundImage: 'url(' + item.thumb_pic + ')' }"
+					></view>
+				</view>
+			</view>
+		</view>
+	</z-paging>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
-			}
+export default {
+	data() {
+		return {
+			album_id: 0,
+			photos: []
+		};
+	},
+	methods: {
+		zQuery: async function(page, pageSize) {
+			const result = await this.$http.get({
+				r: 'user/photolist',
+				albumId: this.album_id,
+				page,
+				pageSize
+			});
+
+			this.$refs.paging.complete(result.list);
 		},
-		methods: {
-			
+		preview: function(e) {
+			uni.previewImage({
+				urls: this.photos.map(item => item.origin_pic),
+				current: e.target.dataset.pic
+			});
 		}
+	},
+	onLoad: function(options) {
+		this.album_id = options.album_id;
+		this.$refs.paging.reload();
 	}
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
