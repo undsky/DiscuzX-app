4f55b757b4335732eaf2614a0d8376e554736adc
jiangyanxi <jiangyanxi@live.com>
2023-12-14 10:09:25 +0800

    板块列表增加分类信息

--- pages/board/list/list.vue
+++ pages/board/list/list.vue
@@ -3,6 +3,15 @@
 		<u-tabs-swiper v-if="tabList.length > 0" ref="tabs" :list="tabList" name="classificationType_name"
 			:current="tabIndex" @change="tabChange">
 		</u-tabs-swiper>
+		<div v-if="classified && classified.length > 0" class="padding-lr-sm padding-top-sm">
+			<scroll-view class="margin-bottom-sm" v-for="(item,index) in classified" :key="index" scroll-x>
+				<text>{{item.classifiedTitle}}:</text>
+				<block v-if="item.classifiedRules && item.classifiedRules.choices">
+					<button v-for="(btnItem,btnIndex) in item.classifiedRules.choices" :key="btnItem.value"
+						class="cu-btn sm margin-left-sm">{{btnItem.name}}</button>
+				</block>
+			</scroll-view>
+		</div>
 		<swiper class="swiper" :current="currentIndex" @transition="transition" @animationfinish="animationfinish">
 			<swiper-item class="swiper-item" v-for="(item, index) in tabList" :key="index">
 				<dx-post-paging :params="{
@@ -24,7 +33,8 @@
 				board_id: 0,
 				tabList: [],
 				tabIndex: 0,
-				currentIndex: 0
+				currentIndex: 0,
+				classified: []
 			}
 		},
 		methods: {
@@ -63,13 +73,14 @@
 			}
 			this.tabList = _tabList;
 
-			console.log('分类信息', result.classificationTop_list)
 			if (result.classificationTop_list && result.classificationTop_list.length > 0) {
 				const classificationTop_info = await this.$http.get({
 					r: 'forum/classification',
 					sortid: result.classificationTop_list[0].classificationTop_id
 				})
-				console.log('分类信息详情', classificationTop_info)
+				if (classificationTop_info && classificationTop_info.body) {
+					this.classified = classificationTop_info.body.classified
+				}
 			}
 		}
 	}
