d73559e08419e240142aca94e300b915b51aa303
姜彦汐 <jiangyanxi@live.com>
2023-12-15 22:39:54 +0800

    修复列表显示2张图片时错位问题

--- components/dx-post-paging/dx-post-paging.vue
+++ components/dx-post-paging/dx-post-paging.vue
@@ -1,6 +1,6 @@
 <template>
 	<view class="content">
-		<z-paging :fixed="false" style="height: 100%;" ref="paging" v-model="zList" :use-page-scroll="usePageScroll"
+		<z-paging :fixed="false" style="height: 100%" ref="paging" v-model="zList" :use-page-scroll="usePageScroll"
 			autowire-query-name="zQuery" :auto="false" :enable-back-to-top="currentIndex === tabIndex">
 			<view class="cu-list" :class="[hasAvatar ? 'menu-avatar' : 'padding-lr']">
 				<block v-for="(item, index) in zList" :key="index">
@@ -12,20 +12,31 @@
 						</view>
 						<view class="content flex-sub justify-center">
 							<view>
-								<view class="text-cut">{{ item.title || item.topic_subject || item.note }}</view>
+								<view class="text-cut">{{
+                  item.title || item.topic_subject || item.note
+                }}</view>
 							</view>
 							<view class="text-cut text-grey text-sm">
-								{{ item.subject || item.reply_content || '' }}
+								{{ item.subject || item.reply_content || "" }}
 								<!-- <mp-html lazy-load :preview-img="false"
 					:content="$util.mobcent.phiz(item.subject || item.reply_content)" /> -->
 							</view>
 							<view class="text-xs flex justify-between">
 								<view class="flex">
 									<view class="text-cut text-green">
-										{{ item.user_nick_name || item.reply_nick_name || item.author }}
+										{{
+                      item.user_nick_name || item.reply_nick_name || item.author
+                    }}
 									</view>
 									<view class="text-gray margin-left-xs">
-										{{ $u.timeFrom(item.last_reply_date || item.replied_date || item.dateline || item.start_date) }}
+										{{
+                      $u.timeFrom(
+                        item.last_reply_date ||
+                          item.replied_date ||
+                          item.dateline ||
+                          item.start_date
+                      )
+                    }}
 									</view>
 								</view>
 								<view v-if="item.board_name || item.hits || item.replies" class="text-gray">
@@ -40,20 +51,23 @@
 					</view>
 					<view v-else class="cu-card dynamic" :data-id="item.topic_id" :data-note="item.note"
 						:data-announce_id="item.announce_id" @click="gotoDetail">
-						<view class="cu-item shadow" style="margin: 0;">
-
+						<view class="cu-item shadow" style="margin: 0">
 							<view class="text-content">
 								{{ item.title || item.topic_subject || item.note }}
 							</view>
 
-							<view class="grid flex-sub padding-lr"
-								:class="item.imageList.length == 1 ? 'col-1' :  item.imageList.length == 2 ? 'col-6 grid-square' : 'col-3 grid-square'">
-								<block v-for="(imgItem,imgIndex) in item.imageList" :key="imgIndex">
-									<view :class="item.imageList.length == 1?'only-img':''" v-if="imgIndex<3"
-										class="bg-img" :style="{backgroundImage:'url('+imgItem+')'}">
+							<view class="grid flex-sub padding-lr" :class="
+                  item.imageList.length == 1
+                    ? 'col-1'
+                    : item.imageList.length == 2
+                    ? 'col-6 grid-square justify-between'
+                    : 'col-3 grid-square'
+                ">
+								<block v-for="(imgItem, imgIndex) in item.imageList" :key="imgIndex">
+									<view :class="item.imageList.length == 1 ? 'only-img' : ''" v-if="imgIndex < 3"
+										class="bg-img" :style="{ backgroundImage: 'url(' + imgItem + ')' }">
 									</view>
 								</block>
-
 							</view>
 						</view>
 						<view class="cu-list menu-avatar">
@@ -64,10 +78,19 @@
 								</view>
 								<view class="content flex-sub justify-center">
 									<view class="text-green">
-										{{ item.user_nick_name || item.reply_nick_name || item.author }}
+										{{
+                      item.user_nick_name || item.reply_nick_name || item.author
+                    }}
 									</view>
 									<view class="text-gray text-sm flex justify-between">
-										{{ $u.timeFrom(item.last_reply_date || item.replied_date || item.dateline || item.start_date) }}
+										{{
+                      $u.timeFrom(
+                        item.last_reply_date ||
+                          item.replied_date ||
+                          item.dateline ||
+                          item.start_date
+                      )
+                    }}
 										<view v-if="item.board_name || item.hits || item.replies" class="text-gray">
 											{{ item.board_name }}
 											<text class="cuIcon-attentionfill margin-lr-xs"></text>
@@ -81,8 +104,6 @@
 						</view>
 					</view>
 				</block>
-
-
 			</view>
 		</z-paging>
 	</view>
@@ -90,42 +111,42 @@
 
 <script>
 	export default {
-		name: 'dx-post-paging',
+		name: "dx-post-paging",
 		props: {
 			params: Object,
 			reload: {
 				type: Boolean,
-				default: false
+				default: false,
 			},
 			hasAvatar: {
 				type: Boolean,
-				default: true
+				default: true,
 			},
 			usePageScroll: {
 				type: Boolean,
-				default: false
+				default: false,
 			},
 			auth: {
 				type: Boolean,
-				default: false
+				default: false,
 			},
 			method: {
 				type: String,
-				default: 'get'
+				default: "get",
 			},
 			tabIndex: {
 				type: Number,
-				default: 0
+				default: 0,
 			},
 			currentIndex: {
 				type: Number,
-				default: 0
-			}
+				default: 0,
+			},
 		},
 		data() {
 			return {
 				zList: [],
-				firstLoaded: false
+				firstLoaded: false,
 			};
 		},
 		watch: {
@@ -142,8 +163,8 @@
 						}
 					}
 				},
-				immediate: true
-			}
+				immediate: true,
+			},
 		},
 		methods: {
 			zQuery: async function(page, pageSize) {
@@ -152,11 +173,11 @@
 					const result = await this.$http[this.method](
 						Object.assign(this.params, {
 							page,
-							pageSize
+							pageSize,
 						}), {
 							custom: {
-								auth: this.auth
-							}
+								auth: this.auth,
+							},
 						}
 					);
 					if (result.list && result.list.length > 0) {
@@ -166,7 +187,7 @@
 					} else if (result.body.data && result.body.data.length > 0) {
 						list = result.body.data;
 					} else if (result.anno_list && result.anno_list.length > 0) {
-						list = result.anno_list
+						list = result.anno_list;
 					}
 				} catch (e) {
 					//TODO handle the exception
@@ -185,8 +206,8 @@
 					this.$util.helper.modal(note);
 				} else if (announce_id) {
 					const result = await this.$http.get({
-						r: 'forum/announcement',
-						id: announce_id
+						r: "forum/announcement",
+						id: announce_id,
 					});
 
 					if (result) {
@@ -197,25 +218,25 @@
 							content: {
 								infor,
 								type
-							}
-						} = result.body.list
+							},
+						} = result.body.list;
 
 						uni.showModal({
 							title,
 							content: infor + `\r\n\n${starttime} 至 ${endtime}`,
 							showCancel: false,
-							cancelText: '',
-							confirmText: '确定',
-							success: res => {},
+							cancelText: "",
+							confirmText: "确定",
+							success: (res) => {},
 							fail: () => {},
-							complete: () => {}
+							complete: () => {},
 						});
 					}
 				} else {
-					this.$util.helper.goto('/pages/detail/detail?id=' + id);
+					this.$util.helper.goto("/pages/detail/detail?id=" + id);
 				}
-			}
-		}
+			},
+		},
 	};
 </script>
 
@@ -231,8 +252,12 @@
 	}
 
 	.grid.col-6.grid-square>uni-view {
-		padding-bottom: calc((100% - 20px)/2);
+		padding-bottom: calc((100% - 20px) / 2);
 		height: 0;
-		width: calc((100% - 20px)/2);
+		width: calc((100% - 20px) / 2);
+	}
+
+	.grid.col-6.grid-square>uni-view:last-child {
+		margin-right: 0 !important;
 	}
 </style>
